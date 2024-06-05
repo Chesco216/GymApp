d7d025af2ba2c0db4cac862d20d36fdb9ae9df23
Chesco216 <franchescofv198@gmail.com>
2024-06-04 22:34:54 -0400

    Profile screen with diet modal

--- src/Screens/Profile.jsx
+++ src/Screens/Profile.jsx
@@ -2,61 +2,67 @@ import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../services/firebase'
 import { useContext, useEffect, useState } from 'react'
 import { userContext } from '../context/UserProvider'
+import { Loading } from '../components/Loading'
+import closeSession from '../services/closeSession'
+import './Profile.css'
+import { DietGrid } from '../components/DietGrid'
 
 export const Profile = () => {
   
   const { userinfo } = useContext(userContext)
   const [user, setUser] = useState(null)
-  
+
   const getUserInfo = async() => {
     const response = await getDoc(doc(db, 'users', userinfo.uid))
     const data = response.data()
     setUser(data.userObj)
-    // console.log('data', data.userObj)
     // return data
   }
-  
-  console.log(user)
-  //NOTE: a component cant be asyncronous
+
   useEffect(() => {
     getUserInfo()
-    // console.log('user', user)
   }, [])
 
   return (
-    <>
-      <div className='user-pic-name-email'>
-        <img src={ user.profilePictureURL } alt='profile-picture'/>
-        <span>
-          <label>{ user.username }</label>
-          <label>{ user.email }</label>
-        </span>
+    <div>
+      { (user) === null ? ( <Loading/> ) 
+      :
+      (
+      <div className='profile-container'>
+        <div className='user-pic-name-email'>
+          <img className='user-image' src={ user.profilePictureURL } alt='profile-picture'/>
+          <span className='user-name-email'>
+            <h1>{ user.username }</h1>
+            <label>{ user.email }</label>
+          </span>
+        </div>
+        <div className='stats-section'>
+          <span className='user-stats-span'>
+            <h1 className='user-stats-value'>{ user.weight }</h1>
+            <label className='user-stats-label'>PESO</label>
+          </span>
+          <span className='user-stats-span'>
+            <h1 className='user-stats-value'>{ user.height }</h1>
+            <label className='user-stats-label'>ALTURA</label>
+          </span>
+          <span className='user-stats-span'>
+            <h1 className='user-stats-value'>{ user.age }</h1>
+            <label className='user-stats-label'>EDAD</label>
+          </span>
+        </div>
+        <div className='profile-diet-content'>
+          <h1>Tu dieta semanal (le ponemos un iconito de comida)</h1>
+          <DietGrid/>
+        </div>
+        <div className='profile-routine-section'>
+          routines content
+        </div>
+        <div className='profile-progress-section'>
+          progress content
+        </div>
+            <button onClick={closeSession}>exit</button>
       </div>
-      <div className='stats-section'>
-        <span>
-          <h1>{ user.weight }</h1>
-          <label>PESO</label>
-        </span>
-        <span>
-          <h1>{ user.height }</h1>
-          <label>ALTURA</label>
-        </span>
-        <span>
-          <h1>{ user.age }</h1>
-          <label>EDAD</label>
-        </span>
-      </div>
-      <div className='profile-diet-content'>
-        dient context
-      </div>
-      <div className='profile-routine-section'>
-        routines content
-      </div>
-      <div className='profile-progress-section'>
-        progress content
-      </div>
-    </>
+      )}
+    </div>
   )
 }
-
-

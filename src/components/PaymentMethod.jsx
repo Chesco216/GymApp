a1a4dabcbb3ffe0595f9b21ef365eaa8765383e8
Yorch466 <jorgellave77@gmail.com>
2024-06-09 23:41:21 -0400

    payment method added without conection to any DB

--- src/components/PaymentMethod.jsx
+++ src/components/PaymentMethod.jsx
@@ -1,4 +1,4 @@
-import './PaymentMethod.css'
+import './PaymentMethod.css';
 import React, { useState } from 'react';
 
 const PaymentMethod = () => {
@@ -33,29 +33,60 @@ const PaymentMethod = () => {
     setPaypalEmail(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    let paymentData;
+
+    switch (paymentMethod) {
+      case 'credit':
+        paymentData = {
+          method: 'credit',
+          creditCardNumber,
+          expirationDate,
+          cvn,
+        };
+        break;
+      case 'debit':
+        paymentData = {
+          method: 'debit',
+          debitCardNumber,
+          expirationDate,
+          cvn,
+        };
+        break;
+      case 'paypal':
+        paymentData = {
+          method: 'paypal',
+          paypalEmail,
+        };
+        break;
+      default:
+        paymentData = {};
+    }
+
+    console.log('Payment Data:', paymentData);
+    // Aquí puedes agregar la lógica para enviar los datos a Firebase
   };
 
   return (
-    <div className="container">
+    <div className="container-payment-card">
       <form onSubmit={handleSubmit} className="payment-container">
-        <div className="form-group promo-code-section">
+        <div className="payment-form-group">
           <label>Código de promoción</label>
-          <input type="text" name="promoCode" />
+          <input type="text" name="promoCode" className="payment-input-field" />
         </div>
-        <div className="form-group gift-cards-rewards-section">
+        <div className="payment-form-group">
           <label>Aplicar Código de regalo</label>
-          <input type="text" name="giftCardsRewards" />
+          <input type="text" name="giftCardsRewards" className="payment-input-field" />
         </div>
-        <div className="payment-options-section">
+        <div>
           <div className="payment-titles">
-            <label className="method-title">Elegir Método de Pago</label>
-            <img src="https://lofrev.net/wp-content/photos/2016/07/visa_logo_7.jpg" alt="visa logo" />
-            <img src="https://www.oceanpayment.com/shopify_app/oceanpayment_logo/Paypal.png" alt="paypal logo" />
+            <label className="payment-method-title">Elegir Método de Pago</label>
+            <img src="https://lofrev.net/wp-content/photos/2016/07/visa_logo_7.jpg" alt="visa logo" className="payment-logo" />
+            <img src="https://www.oceanpayment.com/shopify_app/oceanpayment_logo/Paypal.png" alt="paypal logo" className="payment-logo" />
           </div>
           <div className="payment-options">
-            <div className="option">
+            <div className="payment-option">
               <input
                 type="radio"
                 value="credit"
@@ -64,7 +95,7 @@ const PaymentMethod = () => {
               />
               <label>Credit Card</label>
             </div>
-            <div className="option">
+            <div className="payment-option">
               <input
                 type="radio"
                 value="debit"
@@ -73,7 +104,7 @@ const PaymentMethod = () => {
               />
               <label>Debit Card</label>
             </div>
-            <div className="option">
+            <div className="payment-option">
               <input
                 type="radio"
                 value="paypal"
@@ -85,80 +116,87 @@ const PaymentMethod = () => {
           </div>
         </div>
         {paymentMethod === 'credit' && (
-          <div className="credit-card-details-section">
-            <div className="form-group">
+          <div>
+            <div className="payment-form-group">
               <label>Numero Tarjeta de Credito</label>
               <input
                 type="text"
                 value={creditCardNumber}
                 onChange={handleCreditCardNumberChange}
                 maxLength="16"
+                className="payment-input-field"
               />
             </div>
-            <div className="form-group">
+            <div className="payment-form-group">
               <label>Expiracion MM/YY</label>
               <input
                 type="text"
                 value={expirationDate}
                 onChange={handleExpirationDateChange}
                 maxLength="5"
+                className="payment-input-field"
               />
             </div>
-            <div className="form-group">
+            <div className="payment-form-group">
               <label>CVN</label>
               <input
                 type="text"
                 value={cvn}
                 onChange={handleCvnChange}
                 maxLength="3"
+                className="payment-input-field"
               />
             </div>
           </div>
         )}
         {paymentMethod === 'debit' && (
-          <div className="debit-card-details-section">
-            <div className="form-group">
+          <div>
+            <div className="payment-form-group">
               <label>Numero Tarjeta de Debito</label>
               <input
                 type="text"
                 value={debitCardNumber}
                 onChange={handleDebitCardNumberChange}
                 maxLength="16"
+                className="payment-input-field"
               />
             </div>
-            <div className="form-group">
+            <div className="payment-form-group">
               <label>Expiracion MM/YY</label>
               <input
                 type="text"
                 value={expirationDate}
                 onChange={handleExpirationDateChange}
                 maxLength="5"
+                className="payment-input-field"
               />
             </div>
-            <div className="form-group">
+            <div className="payment-form-group">
               <label>CVN</label>
               <input
                 type="text"
                 value={cvn}
                 onChange={handleCvnChange}
                 maxLength="3"
+                className="payment-input-field"
               />
             </div>
           </div>
         )}
         {paymentMethod === 'paypal' && (
-          <div className="paypal-email-section">
-            <div className="form-group">
+          <div>
+            <div className="payment-form-group">
               <label>PayPal Email</label>
               <input
                 type="email"
                 value={paypalEmail}
                 onChange={handlePaypalEmailChange}
+                className="payment-input-field"
               />
             </div>
           </div>
         )}
-        <button type="submit">Realizar Pago</button>
+        <button type="submit" className="payment-button">Realizar Pago</button>
       </form>
     </div>
   );
